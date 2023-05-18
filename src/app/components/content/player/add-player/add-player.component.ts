d528c761d75d89f55c38e7d649e90570bced2ba0
Med-Marwen-BS <bensalah.medmarwen@gmail.com>
2023-05-18 18:27:57 +0100

    fix html

--- src/app/components/content/player/add-player/add-player.component.ts
+++ src/app/components/content/player/add-player/add-player.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-add-player',
@@ -8,6 +11,47 @@ import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AddPlayerComponent {
   model!:any;
+  isLoggedIn = true;
+
+  player:any={
+    firstName:"",
+    lastName:"",
+    email:"",
+    birthDate:"",
+    phoneNumber:"",
+    height:"",
+    width:"",
+    sexe:""
+    };
+
+  playerForm = new FormGroup({
+    firstName : new FormControl('',[Validators.required]),
+    lastName : new FormControl('',Validators.required),
+    email : new FormControl('',Validators.required),
+    birthDate : new FormControl('',Validators.required),
+    phoneNumber : new FormControl('',Validators.required),
+    height : new FormControl('',Validators.required),
+    width : new FormControl('',Validators.required),
+    sexe : new FormControl('',Validators.required),
+    
+  })
+
+  constructor(private authService:AuthService,private router:Router){}
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLogged();
+    //if(this.isLoggedIn) this.router.navigate(['/listTeam'])
+   
+  }
+
+  onSubmit(){
+
+    if(this.playerForm.valid){
+        
+    }else{
+      this.playerForm.markAllAsTouched();
+
+    }
+  }
 
 
 }
