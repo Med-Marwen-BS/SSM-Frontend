7dd0cba8a8b8dafb0f4c7bf50b0bf1cf59804f6c
Med-Marwen-BS <bensalah.medmarwen@gmail.com>
2023-05-21 02:51:07 +0100

    scoreBoard and stats

--- src/app/components/content/match/live-match/live-match.component.ts
+++ src/app/components/content/match/live-match/live-match.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { CategoryService } from 'src/app/services/category.service';
 import { MatchService } from 'src/app/services/match.service';
+import { PlayerStatService } from 'src/app/services/player-stat.service';
 
 @Component({
   selector: 'app-live-match',
@@ -15,8 +16,9 @@ export class LiveMatchComponent implements OnInit {
   user!:any
   match!:any
   team!:any
+  playerStat!:any
 
-  constructor(private matchService:MatchService,private activatedroute:ActivatedRoute,private categoryService: CategoryService, private authService: AuthService, private router: Router) { }
+  constructor(private playerStatService:PlayerStatService,private matchService:MatchService,private activatedroute:ActivatedRoute,private categoryService: CategoryService, private authService: AuthService, private router: Router) { }
   ngOnInit(): void {
     //this.isLoggedIn = this.authService.isLogged();
     if (!this.isLoggedIn) this.router.navigate(['/login'])
@@ -29,6 +31,9 @@ export class LiveMatchComponent implements OnInit {
       this.matchService.getMatchById(matchId).subscribe(data=>{
         this.match=data
         this.team=data.team
+        this.playerStatService.getMatchById(matchId).subscribe(data=>
+          this.playerStat=data
+          )
       })
     }
     
@@ -42,6 +47,55 @@ export class LiveMatchComponent implements OnInit {
     if(teamCode=='B'){
       this.match.opponentScore +=1
     }
+  }
+
+  incrementShot(playerId:any){
+    let p = this.playerStat.find((p:any)=>playerId==p.player.id)
+    p.shots = p.shots+1
+  }
+  decrementShot(playerId:any){
+    let p = this.playerStat.find((p:any)=>playerId==p.player.id)
+    if(p.shots!=0)
+      p.shots = p.shots-1
+  }
+
+  incrementGoals(playerId:any){
+    let p = this.playerStat.find((p:any)=>playerId==p.player.id)
+    p.goals = p.goals+1
+  }
+  decrementGoals(playerId:any){
+    let p = this.playerStat.find((p:any)=>playerId==p.player.id)
+    if(p.goals!=0)
+      p.goals = p.goals-1
+  }
+
+  incrementSaves(playerId:any){
+    let p = this.playerStat.find((p:any)=>playerId==p.player.id)
+    p.saves = p.saves+1
+  }
+  decrementSaves(playerId:any){
+    let p = this.playerStat.find((p:any)=>playerId==p.player.id)
+    if(p.saves!=0)
+      p.saves = p.saves-1
+  }
+
+  incrementMinutes(playerId:any){
+    let p = this.playerStat.find((p:any)=>playerId==p.player.id)
+    if(p.minutes!=60)
+      p.minutes = p.minutes+1
+  }
+  decrementMinutes(playerId:any){
+    let p = this.playerStat.find((p:any)=>playerId==p.player.id)
+    if(p.minutes!=0)
+      p.minutes = p.minutes-1
+  }
+
+  update(){
+    //TODO : Update
+    // this.playerStat.map((ps:any)=>{
+
+    //   this.playerStatService.
+    // })
   }
 
 
