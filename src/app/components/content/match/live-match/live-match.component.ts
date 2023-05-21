b1ff0d164962efc7a6bc3d070b77a3b873ada4aa
Med-Marwen-BS <bensalah.medmarwen@gmail.com>
2023-05-21 01:33:16 +0100

    match board

--- src/app/components/content/match/live-match/live-match.component.ts
+++ src/app/components/content/match/live-match/live-match.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { MatchService } from 'src/app/services/match.service';
 
 @Component({
   selector: 'app-live-match',
   templateUrl: './live-match.component.html',
   styleUrls: ['./live-match.component.css']
 })
-export class LiveMatchComponent {
+export class LiveMatchComponent implements OnInit {
+
+  isLoggedIn = true;
+  user!:any
+  match!:any
+  team!:any
+
+  constructor(private matchService:MatchService,private activatedroute:ActivatedRoute,private categoryService: CategoryService, private authService: AuthService, private router: Router) { }
+  ngOnInit(): void {
+    //this.isLoggedIn = this.authService.isLogged();
+    if (!this.isLoggedIn) this.router.navigate(['/login'])
+    this.authService.getUserByToken().subscribe(data => {
+      this.user = data.data;
+
+    });
+    let matchId=this.activatedroute.snapshot.params['id'];
+    if(matchId != null){
+      this.matchService.getMatchById(matchId).subscribe(data=>{
+        this.match=data
+        this.team=data.team
+      })
+    }
+    
+
+  }
+
+  incrementScore(teamCode:any){
+    if(teamCode=='A'){
+      this.match.myScore +=1
+    }
+    if(teamCode=='B'){
+      this.match.opponentScore +=1
+    }
+  }
+
 
 }
